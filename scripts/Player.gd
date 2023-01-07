0a94cde6aefb7516b52ed13377c49e8bc9a04c1d
WillYum <liamshere98@gmail.com>
2023-01-07 17:39:18 +0200

    Implement baisc grid movement bounds for player

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -4,31 +4,45 @@ extends Sprite
 signal moved(at_timestamp)
 
 var line = 0
-var direction = Vector2.ZERO
+# var move_direction = Vector2.ZERO
+
+# var current_cell_position = Vector2(0, 0)
+var min_cell_position = Vector2(0, 0);
+var max_cell_position = Vector2(5, 5);
 
 
 func _ready():
 	pass # Replace with function body.
 
 
-func get_input():
-	direction = Vector2.ZERO
+func get_input() -> Vector2:
+	var move_direction = Vector2.ZERO
 
 	if Input.is_action_just_pressed("ui_right"):
-		direction = Vector2(1, 0)
+		move_direction = Vector2(1, 0)
 	elif Input.is_action_just_pressed("ui_left"):
-		direction = Vector2(-1, 0)
+		move_direction = Vector2(-1, 0)
 	elif Input.is_action_just_pressed("ui_down"):
-		direction = Vector2(0, 1)
+		move_direction = Vector2(0, 1)
 	elif Input.is_action_just_pressed("ui_up"):
-		direction = Vector2(0, -1)
+		move_direction = Vector2(0, -1)
+
+	return move_direction;
 
 
 func _process(_delta):
-	get_input()
+	var newMoveDir = get_input()
 
-	if direction != Vector2.ZERO:
+	
+	if newMoveDir != Vector2.ZERO:
 		var timestamp = OS.get_ticks_msec()
 		emit_signal("moved", timestamp)
+		
+		var newPosition = global_position + (newMoveDir * globals.cell_size);
 	
-	position += direction * globals.cell_size
+		# print("player position: ", global_position);
+	
+		global_position.x = clamp(newPosition.x, 80, 208);
+		global_position.y = clamp(newPosition.y, 8, 136);
+
+		# print("newPosition", newPosition);
