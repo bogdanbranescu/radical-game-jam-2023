551bc86b0f8931b3ee9f7361121fc8bed67db152
Bogdan <bogdan.g.branescu@gmail.com>
2023-01-07 19:13:57 +0200

    add timestamps

--- _globals.gd
+++ _globals.gd
@@ -8,6 +8,10 @@ var depletion_rate = 5
 
 var cell_size = 32
 
+var timestamps = [
+	[0, 2526, 3474, 4737, 7579, 8526, 9789, 11842, 12316, 12632, 14211, 14842, 15158, 16105]
+]
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,3 +21,4 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
